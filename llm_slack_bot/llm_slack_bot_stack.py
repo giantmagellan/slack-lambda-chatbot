0589d6a781499421730aad1d4492746231d9701b
bryan <bryanflores@sandiego.edu>
2024-11-25 21:38:31 -0800

    function renaming

--- llm_slack_bot/llm_slack_bot_stack.py
+++ llm_slack_bot/llm_slack_bot_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-class IntuitLlmSlackBotStack(Stack):
+class LlmSlackBotStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
--- tests/unit/_llm_slack_bot_stack.py
+++ tests/unit/_llm_slack_bot_stack.py
@@ -1,13 +1,13 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from intuit_llm_slack_bot.intuit_llm_slack_bot_stack import IntuitLlmSlackBotStack
+from llm_slack_bot.llm_slack_bot_stack import LlmSlackBotStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in intuit_llm_slack_bot/intuit_llm_slack_bot_stack.py
 def test_sqs_queue_created():
     app = core.App()
-    stack = IntuitLlmSlackBotStack(app, "intuit-llm-slack-bot")
+    stack = LlmSlackBotStack(app, "llm-slack-bot")
     template = assertions.Template.from_stack(stack)
 
 #     template.has_resource_properties("AWS::SQS::Queue", {
